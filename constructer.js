2a5d01a577e728a00bc2e307997acb086e2a1676
Fidelya1988 <oleksandram1988@gmail.com>
2021-01-22 21:51:44 +0200

    first commit

---
+++ constructer.js
@@ -0,0 +1,9 @@
+
+
+let DataBase = function(id, name, lastName, phoneNumber, city) {
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+
+}
