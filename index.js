e9f6db4f436d119a2be8636a766b034106e01f77
Fidelya1988 <oleksandram1988@gmail.com>
2021-03-09 22:51:47 +0200

    форматирование

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
 
 class User {
-    
-   
-    constructor (id, name, lastName, phone, city) {
-    this.id = id;
-    this.name = name;
-    this.lastName = lastName;
-    this.phone = phone;
-    this.city = city;
-    
+
+
+    constructor(id, name, lastName, phone, city) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.phone = phone;
+        this.city = city;
+
     }
-    
-  
-    
+
+
+
 }
 
 
@@ -21,13 +21,13 @@ class User {
 
 
 let users = [
-new User(1, "Dmytriy", "Parzhytsky", "0931230000", "Bachmut"), 
-new User(2, "Oleksandra", "Marchenko", "0962782865", "Kyiv"), 
-new User(3, "Volodymyr", "Kyselyov", "0962782845", "Kamyans`ke")];
+    new User(1, "Dmytriy", "Parzhytsky", "0931230000", "Bachmut"),
+    new User(2, "Oleksandra", "Marchenko", "0962782865", "Kyiv"),
+    new User(3, "Volodymyr", "Kyselyov", "0962782845", "Kamyans`ke")];
 
-let addTable = users.map (user => {
-    
-  
+let addTable = users.map(user => {
+
+
     let id = `<div class='id'>${user.id}</div>`;
     let name = `<div class='name'>${user.name}</div>`;
     let lastName = `<div class='lastName'>${user.lastName}</div>`;
@@ -35,8 +35,8 @@ let addTable = users.map (user => {
     let city = `<div class='city'>${user.city}</div>`;
 
     let line = $("#table tr:last").after(`<tr><td>${name}</td><td>${lastName}</td><td>${phone}</td><td>${id}</td><td>${city}</td></tr>`);
-    return line;  
-   
- 
+    return line;
+
+
 });
 
