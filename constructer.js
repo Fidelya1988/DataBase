50bb7babbbc369448a8fd3598cc232863d8fd43b
Fidelya1988 <oleksandram1988@gmail.com>
2021-03-09 22:43:32 +0200

    Сделала возможным добавлять юзеров через массив users

--- constructer.js
+++
@@ -1,9 +0,0 @@
-
-
-let DataBase = function(id, name, lastName, phoneNumber, city) {
-    this.id = id;
-    this.name = name;
-    this.lastName = lastName;
-    this.phoneNumber = phoneNumber;
-
-}
---
+++ index.js
@@ -0,0 +1,42 @@
+
+
+class User {
+    
+   
+    constructor (id, name, lastName, phone, city) {
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.city = city;
+    
+    }
+    
+  
+    
+}
+
+
+
+
+
+let users = [
+new User(1, "Dmytriy", "Parzhytsky", "0931230000", "Bachmut"), 
+new User(2, "Oleksandra", "Marchenko", "0962782865", "Kyiv"), 
+new User(3, "Volodymyr", "Kyselyov", "0962782845", "Kamyans`ke")];
+
+let addTable = users.map (user => {
+    
+  
+    let id = `<div class='id'>${user.id}</div>`;
+    let name = `<div class='name'>${user.name}</div>`;
+    let lastName = `<div class='lastName'>${user.lastName}</div>`;
+    let phone = `<div class='phone'>${user.phone}</div>`;
+    let city = `<div class='city'>${user.city}</div>`;
+
+    let line = $("#table tr:last").after(`<tr><td>${name}</td><td>${lastName}</td><td>${phone}</td><td>${id}</td><td>${city}</td></tr>`);
+    return line;  
+   
+ 
+});
+
