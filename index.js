68860c786ff608294d51f9ed6b722f2d734a827b
Fidelya1988 <oleksandram1988@gmail.com>
2021-04-02 02:11:05 +0300

    last version 2.0

--- index.js
+++ index.js
@@ -2,36 +2,31 @@ let form = document.querySelector('form');
 let inputArea = form.querySelector('.input-area');
 let submitButton = form.querySelector('.submit-button');
 let table = document.querySelector('#table');
+let templateTable = document.querySelector('#template-table').content;
+let templateRow = templateTable.querySelector('.template-row')
+let rowId = templateRow.querySelector('.row-id');
+let rowName = templateRow.querySelector('.row-name');
+let rowLastName = templateRow.querySelector('.row-last-name');
+let rowPhone = templateRow.querySelector('.row-phone');
+let rowCity = templateRow.querySelector('.row-city');
 
-
-let templateTable =  document.querySelector('#template-table').content;
-let templateLine = templateTable.querySelector('.template-line')
-let lineId = templateLine.querySelector('.line-id');
-let lineName = templateLine.querySelector('.line-name');
-let lineLastName = templateLine.querySelector('.line-last-name');
-let linePhone = templateLine.querySelector('.line-phone');
-let lineCity = templateLine.querySelector('.line-city');
-let User =  function (id, name, lastName, phone, city){
-
-
-  
-        this.id = id;
-        this.name = name;
-        this.lastName = lastName;
-        this.phone = phone;
-        this.city = city;
-       
+let User = function (id, name, lastName, phone, city) {
 
 
 
+    this.id = id;
+    this.name = name;
+    this.lastName = lastName;
+    this.phone = phone;
+    this.city = city;
 }
 
-const renderLine = (newUser) =>{
-    lineId.textContent  = newUser.id;
-    lineName.textContent = newUser.name;
-    lineLastName.textContent = newUser.lastName;
-    linePhone.textContent = newUser.phone;
-    lineCity.textContent = newUser.city;
+const renderRow = (newUser) => {
+    rowId.textContent = newUser.id;
+    rowName.textContent = newUser.name;
+    rowLastName.textContent = newUser.lastName;
+    rowPhone.textContent = newUser.phone;
+    rowCity.textContent = newUser.city;
 }
 
 
@@ -40,35 +35,31 @@ let users = [];
 
 
 
-console.log(templateLine)
 
+submitButton.addEventListener('click', evt => {
 
-submitButton.addEventListener ('click', evt => {
-    
     evt.preventDefault();
     let inputName = form.querySelector('#name').value;
-    let  inputLastName = form.querySelector('#last-name').value;
-    let  inputPhone = form.querySelector('#phone-number').value;
-    let  inputCity = form.querySelector('#city').value;
-    
-  let newUser =  new User(users.length+1, inputName, inputLastName, inputPhone, inputCity);
- renderLine(newUser);
-  users.push(newUser);
-  console.log(users)
+    let inputLastName = form.querySelector('#last-name').value;
+    let inputPhone = form.querySelector('#phone-number').value;
+    let inputCity = form.querySelector('#city').value;
 
-  
-let newLine = templateLine.cloneNode(true);
-table.appendChild(newLine);
+    let newUser = new User(users.length + 1, inputName, inputLastName, inputPhone, inputCity);
+    renderRow(newUser);
+    users.push(newUser);
+   
+    let newRow = templateRow.cloneNode(true);
+    table.appendChild(newRow);
 
 
-        
-     
+
+
 
 })
 
-  
 
-// let addLine = (newUser) => {
+
+// let addrow = (newUser) => {
 
 //     newUser.map(user => {
 
@@ -78,11 +69,11 @@ table.appendChild(newLine);
 //    let lastName = `<div class='lastName'>${user.lastName}</div>`;
 //    let phone = `<div class='phone'>${user.phone}</div>`;
 //    let city = `<div class='city'>${user.city}</div>`;
- 
-//  let line = $("#table tr:last").after(`<tr><td>${name}</td><td>${lastName}</td><td>${phone}</td><td>${id}</td><td>${city}</td></tr>`);
+
+//  let row = $("#table tr:last").after(`<tr><td>${name}</td><td>${lastName}</td><td>${phone}</td><td>${id}</td><td>${city}</td></tr>`);
 
 
-//   return line;
+//   return row;
 
 
 // })
