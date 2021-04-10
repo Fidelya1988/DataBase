7fb90780cf016f23cbc7c527512ba0f773835d8c
Fidelya1988 <67442054+Fidelya1988@users.noreply.github.com>
2021-04-10 22:31:21 +0300

    Revert "Unify spaces and indentation"
    This reverts commit cc9e18a015aa7a5906e0ee168d25fbec24dc1ef6.

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
-let form = document.querySelector("form");
-let inputAreas = document.querySelectorAll(".input-area");
-let submitButton = form.querySelector(".submit-button");
-let table = document.querySelector("#table");
-let templateTable = document.querySelector("#template-table").content;
-let templateRow = templateTable.querySelector(".template-row");
-let rowId = templateRow.querySelector(".row-id");
-let rowName = templateRow.querySelector(".row-name");
-let rowLastName = templateRow.querySelector(".row-last-name");
-let rowPhone = templateRow.querySelector(".row-phone");
-let rowCity = templateRow.querySelector(".row-city");
-
+let form = document.querySelector('form');
+let inputAreas = document.querySelectorAll('.input-area');
+let submitButton = form.querySelector('.submit-button');
+let table = document.querySelector('#table');
+let templateTable = document.querySelector('#template-table').content;
+let templateRow = templateTable.querySelector('.template-row')
+let rowId = templateRow.querySelector('.row-id');
+let rowName = templateRow.querySelector('.row-name');
+let rowLastName = templateRow.querySelector('.row-last-name');
+let rowPhone = templateRow.querySelector('.row-phone');
+let rowCity = templateRow.querySelector('.row-city');
+console.log(inputAreas)
 let User = function (id, name, lastName, phone, city) {
+
+
+
     this.id = id;
     this.name = name;
     this.lastName = lastName;
     this.phone = phone;
     this.city = city;
-};
+}
 
 const renderRow = (newUser) => {
     rowId.textContent = newUser.id;
@@ -24,32 +27,57 @@ const renderRow = (newUser) => {
     rowLastName.textContent = newUser.lastName;
     rowPhone.textContent = newUser.phone;
     rowCity.textContent = newUser.city;
-};
+}
+
+
 
 let users = [];
 
-submitButton.addEventListener("click", (evt) => {
+
+
+
+submitButton.addEventListener('click', evt => {
+
     evt.preventDefault();
+    let inputName = form.querySelector('#name').value;
+    let inputLastName = form.querySelector('#last-name').value;
+    let inputPhone = form.querySelector('#phone-number').value;
+    let inputCity = form.querySelector('#city').value;
 
-    let inputName = form.querySelector("#name").value;
-    let inputLastName = form.querySelector("#last-name").value;
-    let inputPhone = form.querySelector("#phone-number").value;
-    let inputCity = form.querySelector("#city").value;
-
-    let newUser = new User(
-        users.length + 1,
-        inputName,
-        inputLastName,
-        inputPhone,
-        inputCity
-    );
+    let newUser = new User(users.length + 1, inputName, inputLastName, inputPhone, inputCity);
     renderRow(newUser);
     users.push(newUser);
-
+   
     let newRow = templateRow.cloneNode(true);
     table.appendChild(newRow);
-
-    for (let i = 0; i < inputAreas.length; i++) {
-        inputAreas[i].value = "";
+ 
+    for (let i = 0; i<inputAreas.length; i++) {
+       
+        inputAreas[i].value = '';
     }
-});
+
+
+
+})
+
+
+
+// let addrow = (newUser) => {
+
+//     newUser.map(user => {
+
+
+//    let id = `<div class='id'>${user.id}</div>`;
+//    let name = `<div class='name'>${user.name}</div>`;
+//    let lastName = `<div class='lastName'>${user.lastName}</div>`;
+//    let phone = `<div class='phone'>${user.phone}</div>`;
+//    let city = `<div class='city'>${user.city}</div>`;
+
+//  let row = $("#table tr:last").after(`<tr><td>${name}</td><td>${lastName}</td><td>${phone}</td><td>${id}</td><td>${city}</td></tr>`);
+
+
+//   return row;
+
+
+// })
+// }
