a8f75f5df4536ca6ddc7c19f62661d0a9909c07c
Fidelya1988 <oleksandram1988@gmail.com>
2021-04-12 01:31:47 +0300

    Сделала рефакторинг формы изменений, сделала populate для формы изменения, сделала стилизацию для формы изменеия и таблицы после вызова формы

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ const tbody = document.querySelector("#tbody");
 const tableRowTemplate = document.querySelector("#table-row").content;
 const templateCells = tableRowTemplate.querySelectorAll("td");
 
+const changeUserForm = document.querySelector("#change-user-form");
+const changeInputName = changeUserForm.querySelector("input.name");
+const changeInputLastName = changeUserForm.querySelector("input.last-name");
+const changeInputPhone = changeUserForm.querySelector("input.phone");
+const changeInputCity = changeUserForm.querySelector("input.city");
+const changeFormInputs = changeUserForm.querySelectorAll("input");
+
+const addUserFormInputs = addUserForm.querySelectorAll('input')
+const addUserFormButton = addUserForm.querySelector('button')
 const users = {};
 
 class User {
     static createID() {
-        return Math.random().toString().slice(10);
+        return Math.random().toString().substr(2, 10);
     }
 
     constructor(name, lastName, phone, city) {
@@ -26,11 +35,11 @@ class User {
 
 const createRow = (newUser) => {
     // populate template cells with new content
-    for (const cell of templateCells)
+    for (const cell of templateCells) {
         cell.textContent = newUser[cell.dataset.userPropertyKey];
-      
-        
 
+
+    }
     // return a deep copy of the row
     const row = tableRowTemplate.cloneNode(true).querySelector(".user-row");
 
@@ -49,25 +58,84 @@ addUserForm.addEventListener("submit", (evt) => {
         inputCity.value,
     );
 
- const id = newUser.id;
+    const id = newUser.id;
 
     users[id] = newUser;
-     
+
     const newRow = createRow(newUser);
     tbody.appendChild(newRow);
     addUserForm.reset();
 });
 
-tbody.addEventListener("click", (event) => {
-    // this forbids putting tables inside tables
-    const row = event.target.closest("tr");
+let selectedRow = null;
+let user = null;
+
+const populateChangeUserForm = (row) => {
+    user = users[row.dataset.userId];
+    const userKeys = Object.keys(user)
+    
+    
+    for (const input of changeFormInputs) {
+         
+        const inputPropertyKey = input.dataset.inputPropertyKey;
+        for (i=0; i<userKeys.length; i++) {
+            if (userKeys[i] === inputPropertyKey ) {
+                input.value = user[userKeys[i]]
+            }
+        }
+           
+            }
+        }
    
-    const user = users[row.dataset.userId];
 
-    // TODO: add popup form
-    user.name = prompt("Set new user name:", user.name) || user.name;
+tbody.addEventListener("click", (event) => {
+    event.preventDefault();
 
-    const nameCell = row.querySelector(".name");
-
-    nameCell.textContent = user.name;
+    selectedRow = event.target.closest("tr");
+    populateChangeUserForm(selectedRow)
+    
+    changeUserForm.classList.remove('hidden');
+    changeUserForm.classList.add('show');
+    addUserForm.classList.add('disabled');
+   
+   for (const input of addUserFormInputs) {
+   input.readOnly =true;
+   }
+   addUserFormButton.disabled = true;
+  
 });
+
+changeUserForm.addEventListener("submit", (evt) => {
+    evt.preventDefault();
+    const selectedCells = selectedRow.querySelectorAll("td");
+  
+
+    for (const input of changeFormInputs) {
+
+        const propertyKey = input.dataset.inputPropertyKey;
+        if (user[propertyKey] = propertyKey) {
+            user[propertyKey] = input.value;
+        }
+
+
+    }
+
+    for (const cell of selectedCells) {
+
+        cell.textContent = user[cell.dataset.userPropertyKey]
+    }
+
+
+
+
+    selectedRow = null;
+    changeUserForm.classList.add('hidden');
+    changeUserForm.classList.remove('show');
+    addUserForm.classList.remove('disabled');
+    for (const input of addUserFormInputs) {
+        input.readOnly =false;
+        }
+        addUserFormButton.disabled = false;
+    changeUserForm.reset();
+})
+
